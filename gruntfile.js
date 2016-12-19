94c7484d415546a720a53ee78d0cd1a3df819b2a
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-19 14:30:09 -0500

    Added necessary build dependencies for grunt to clean copy and concat into new build folder.

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,8 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 
+    clean: ['build/'],
+
     jshint: {
       options: {
         jshintrc: '.jshintrc',
@@ -13,11 +15,59 @@ module.exports = function(grunt) {
           src: [ 'src/javascript/**/*.js' ]
         }
       }
+    },
+
+    copy: {
+      html: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: 'index.html',
+            dest: 'build/'
+          },
+        ]
+      },
+
+      vendorjs: {
+        files: [
+          {
+            expand: true,
+            cwd: 'node_modules/angular/',
+            src: ['angular.js'],
+            dest: 'build/javascript/'
+          },
+        ]
+      }
+    },
+
+    concat: {
+      javascript: {
+        src: ['src/javascript/**/*.js'],
+        dest: 'build/javascript/app.js'
+      }
+    },
+
+    watch: {
+      html: {
+        files: ['src/index.html'],
+        tasks: ['copy:html']
+      },
+      js: {
+        files: ['src/javascript/**/*.js'],
+        tasks: ['test', 'concat']
+      },
     }
+
   });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['clean','jshint', 'copy', 'concat']);
 
-  };
+};
