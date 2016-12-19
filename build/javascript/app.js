97c46cbbc5220e7d4abbc7df48d098fb24d6f2f5
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-19 16:22:12 -0500

    included a css folder for the build.

--- build/javascript/app.js
+++ build/javascript/app.js
@@ -1,71 +1,92 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('transport')
-        .controller('RailViewController', RailViewController);
+  angular.module('transport')
+  .controller('RailViewController', RailViewController);
 
-    RailViewController.$inject = ['RailViewService'];
+  RailViewController.$inject = ['RailViewService'];
 
-    function RailViewController(RailViewService) {
+  function RailViewController(RailViewService) {
 
-        this.railInfo = RailViewService.railInfo()
-            .then(function success(data) {
-                console.log('You got data!', data);
-            })
-            .catch(function failure(xhr) {
-                console.log('No data for you :(', xhr);
-            });
+    this.railInfo = RailViewService.railInfo()
+      .then(function success(data) {
+      console.log('Rail Info', data);
+      })
+      .catch(function failure(xhr) {
+      console.log('No data for you :(', xhr);
+      });
 
-    }
+    this.railPark = RailViewService.railParking()
+      .then(function success(data) {
+      console.log('Rail Parking', data);
+      })
+      .catch(function failed(xhr) {
+      console.log('No data for you :(', xhr);
+      });
+
+  }
 
 
 }());
 
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('transport')
-        .factory('RailViewService', RailViewService);
+  angular.module('transport')
+    .factory('RailViewService', RailViewService);
 
-    RailViewService.$inject = ['$http'];
+  RailViewService.$inject = ['$http'];
 
-    function RailViewService($http) {
-        return {
-            railInfo: railInfo
-        };
+  function RailViewService($http) {
+    return {
+      railInfo: railInfo,
+      railParking: railParking
+    };
 
-        function railInfo() {
-            return $http({
-                url: 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03',
-                method: 'get',
-                headers: {
-                    'content-type': 'application/json',
-                    'api_key': 'f44ffd8ba84f459796d5a0870957bdb7'
-                }
-            });
+    function railInfo() {
+      return $http({
+        url: 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/B03',
+        method: 'get',
+        headers: {
+          'content-type': 'application/json',
+          'api_key': 'f44ffd8ba84f459796d5a0870957bdb7'
         }
+      });
     }
 
-}());
-(function() {
-    'use strict';
+    function railParking() {
+      return $http({
+        url:'https://api.wmata.com/Rail.svc/json/jStationParking?StationCode=E08',
+        method:'get',
+        headers: {
+          'content-type': 'application/json',
+          'api_key': 'f44ffd8ba84f459796d5a0870957bdb7'
+        }
+      });
+    }
+  }
 
-    angular.module('transport', []);
-    //   .config(viewConfig);
-    //
-    // viewConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-    //
-    // function viewConfig($stateProvider, $urlRouterProvider) {
-    //   $urlRouterProvider.when('', '/');
-    //
-    //   $stateProvider
-    //     .state({
-    //       name: 'Home',
-    //       url: '/',
-    //       templateUrl: 'src/index.html'
-    //     });
-    //
-    //
-    // }
+}());
+
+(function() {
+  'use strict';
+
+  angular.module('transport', []);
+  // .config(viewConfig);
+  //
+  // viewConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  //
+  // function viewConfig($stateProvider, $urlRouterProvider) {
+  //   $urlRouterProvider.when('', '/');
+  //
+  //   $stateProvider
+  //   .state({
+  //     name: 'home',
+  //     url: '/',
+  //     templateUrl: 'src/views/home.template.html'
+  //   });
+  //
+  //
+  // }
 
 }());
