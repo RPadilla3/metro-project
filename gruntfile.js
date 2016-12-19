d5663ba39ead2c13fa99987e3f8d173f0616acd1
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-19 09:45:56 -0500

    Created the boiler plate for the app, everything is connected and working properly.

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        ignores: ['node_modules/**']
+      },
+      source: {
+        files: {
+          src: [ 'src/javascript/**/*.js' ]
+        }
+      }
+    }
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', ['jshint']);
+
+  });
