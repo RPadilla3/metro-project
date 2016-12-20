0f88d8efc7674188ec9804702d4b45a6307f2e13
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-20 16:28:36 -0500

    Updated build folder.

--- build/javascript/app.js
+++ build/javascript/app.js
@@ -21,19 +21,19 @@
 
 }());
 
-(function() {
-  'use strict';
-
-    function initMap() {
-      var map;
-        map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.1199273249, lng: -77.1646273343},
-        zoom: 10
-      });
-        return initMap();
-    }
-
-}());
+// (function() {
+//   'use strict';
+//
+//     function initMap() {
+//       var map;
+//         map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 39.1199273249, lng: -77.1646273343},
+//         zoom: 10
+//       });
+//         return initMap();
+//     }
+//
+// }());
 
 (function() {
   'use strict';
