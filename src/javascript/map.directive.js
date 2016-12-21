3c2ceae1eca66f8869e20c9e00ad601ee60ce08f
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-21 14:25:36 -0500

    Added the dc map onto the screen to show only on the metro view using Mapbox.

---
+++ src/javascript/map.directive.js
@@ -0,0 +1,20 @@
+(function() {
+ 'use strict';
+
+ angular.module('transport')
+   .directive('mapbox', MapBox);
+
+
+   function MapBox() {
+     return {
+       restrict: 'EA',
+       link: function (scope, element) {
+         console.log(element);
+         L.mapbox.accessToken = 'pk.eyJ1IjoicnBhZGlsbGEzIiwiYSI6ImNpd3hrZjF4MTAwN20ydW82ODNyOHp3Z2UifQ.ATqkcRlunPfsvsS5SGFM6Q';
+         L.mapbox.map(element[0], 'mapbox.streets')
+           .setView([38.9072, -77.0369], 13.2);
+       }
+     };
+   }
+
+}());
