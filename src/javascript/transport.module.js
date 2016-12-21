279ea121c62adce0e7f3233201571330b9eb6252
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-21 10:26:19 -0500

    Connected all of the views together.

--- src/javascript/transport.module.js
+++ src/javascript/transport.module.js
@@ -1,22 +1,33 @@
 (function() {
   'use strict';
 
-  angular.module('transport', []);
-  // .config(viewConfig);
-  //
-  // viewConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-  //
-  // function viewConfig($stateProvider, $urlRouterProvider) {
-  //   $urlRouterProvider.when('', '/');
-  //
-  //   $stateProvider
-  //   .state({
-  //     name: 'home',
-  //     url: '/',
-  //     templateUrl: 'src/views/home.template.html'
-  //   });
-  //
-  //
-  // }
+  angular.module('transport', ['ui.router'])
+  .config(viewConfig);
+
+  viewConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function viewConfig($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.when('', '/');
+
+    $stateProvider
+    .state({
+      name: 'home',
+      url: '',
+      templateUrl: '../views/home.template.html'
+    })
+    .state({
+      name: 'metro',
+      url: '/metro',
+      templateUrl: '../views/metro.template.html'
+    })
+    .state({
+      name: 'login',
+      url: '/login',
+      templateUrl: '../views/login.template.html'
+    });
+
+
+  }
 
 }());
