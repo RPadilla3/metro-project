d17214291dee682971c0bdad38fb9ab5bf2da430
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-22 11:31:04 -0500

    Created a basic test for the RailViewController to get testing started.

--- test/specs/rail-view.controller.spec.js
+++ test/specs/rail-view.controller.spec.js
@@ -1,4 +1,20 @@
 (function() {
   'use strict';
-  
+
+  var expect = chai.expect;
+
+  describe('Rail-View Controller', function() {
+    var RailViewController;
+    var mockRailViewService = {};
+
+    beforeEach(module('transport'));
+    beforeEach(inject(function($controller) {
+      RailViewController = $controller('RailViewController');
+    }))
+
+    it('Should contain scope variables', function() {
+      expect(RailViewController).to.be.an('Object');
+    })
+  });
+
 }());
