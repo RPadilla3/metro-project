c89dfdc271382695020b39b47fea632350235aaf
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2017-01-09 14:01:49 -0500

    Started testing for the controller.

--- test/specs/rail-view.controller.spec.js
+++ test/specs/rail-view.controller.spec.js
@@ -1,4 +1,40 @@
 (function() {
   'use strict';
-  
+
+  var expect = chai.expect;
+
+  describe('RailViewController', function() {
+    var RailViewController;
+    var $scope = {};
+    var $rootScope;
+    var mockRailViewService = {};
+
+
+  beforeEach(module('transport'));
+
+  beforeEach(module(function($provide){
+    $provide.value('RailViewService', mockRailViewService);
+
+  }));
+
+  beforeEach(inject(function($controller, $q,_$rootScope_) {
+    $rootScope = _$rootScope_;
+    mockRailViewService.railInfo = function() {
+      console.log('Mock railInfo() on RailViewService');
+      return $q.resolve({
+        "Incidents": [
+          {
+            "Description": "Red Line: Expect residual delays to Shady Grove due to an earlier signal problem at Farragut North.",
+            "PassengerDelay": 0,
+            "IncidentType": "Delay",
+            "LinesAffected": "RD;"
+          }
+        ]
+      });
+    };
+    RailViewController = $controller('RailViewController', {$scope: $scope});
+  }));
+
+});
+
 }());
