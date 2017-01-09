ba1682bde5489aa95ac5411a26dd31bbcce2e02e
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2017-01-09 11:32:18 -0500

    Got first successful test.

--- test/specs/rail-view.service.spec.js
+++ test/specs/rail-view.service.spec.js
@@ -11,34 +11,48 @@
 
     beforeEach(module('transport'));
 
-    beforeEach(inject(function(_$httpBackend_,_RailViewService_) {
+    beforeEach(inject(function(_$httpBackend_,_RailViewService_,_$rootScope_) {
       $httpBackend = _$httpBackend_;
       RailViewService = _RailViewService_;
+      $rootScope = _$rootScope_;
+
+
+      $httpBackend
+      .whenGET('https://api.wmata.com/Incidents.svc/json/Incidents')
+      .respond({
+        "Incidents": [
+          {
+            "IncidentID": "01C30D6E-8E5E-4B06-9D47-2E277530DE5F",
+            "Description": "Red Line: Expect residual delays to Shady Grove due to an earlier signal problem at Farragut North.",
+            "StartLocationFullName": null,
+            "EndLocationFullName": null,
+            "PassengerDelay": 0,
+            "DelaySeverity": null,
+            "IncidentType": "Delay",
+            "EmergencyText": null,
+            "LinesAffected": "RD;",
+            "DateUpdated": "2017-01-09T10:58:29"
+          }
+        ]
+      });
+
+      $httpBackend
+      .whenGET('views/home.template.html')
+      .respond('what yo');
+
     }));
 
-    $httpBackend
-    .whenGet('https://api.wmata.com/Incidents.svc/json/Incidents')
-    .respond({
-      "Incidents": [
-        {
-          "IncidentID": "01C30D6E-8E5E-4B06-9D47-2E277530DE5F",
-          "Description": "Red Line: Expect residual delays to Shady Grove due to an earlier signal problem at Farragut North.",
-          "StartLocationFullName": null,
-          "EndLocationFullName": null,
-          "PassengerDelay": 0,
-          "DelaySeverity": null,
-          "IncidentType": "Delay",
-          "EmergencyText": null,
-          "LinesAffected": "RD;",
-          "DateUpdated": "2017-01-09T10:58:29"
-        }
-      ]
+    it('should be able to GET a list of metro delays', function(done) {
+      var result = RailViewService.railInfo();
+      expect(result).to.be.a('object');
+      done();
     });
 
 
 
-    $httpBackend.flush();
 
-    });
+    // $httpBackend.flush();
 
-  }());
+  });
+
+}());
